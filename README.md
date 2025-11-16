a22892e4366dadcb134e4a27f32e3d531de5c37b
Anton Statut <antonstatut@gmail.com>
2025-11-16 15:31:33 +0300

    добавил бейдж в рейдми

---
+++ README.md
@@ -0,0 +1,1 @@
+[![CI](https://github.com/antonstatut/Matchers/actions/workflows/main.yml/badge.svg)](https://github.com/antonstatut/Matchers/actions/workflows/main.yml)
--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -1,9 +1,9 @@
 import sortHeroes from '../app.js';
 
 const heroes = [
-  {name: 'мечник', health: 10},
-  {name: 'маг', health: 100},
-  {name: 'лучник', health: 80},
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
 ];
 
 const correctHeroes = [
