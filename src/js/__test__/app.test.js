37316b5218c0c87aace43db4ef37ba08d40d4404
Anton Statut <antonstatut@gmail.com>
2025-11-16 09:45:04 +0300

    initial commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,18 @@
+import sortHeroes from '../app.js';
+
+const heroes = [
+  {name: 'мечник', health: 10},
+  {name: 'маг', health: 100},
+  {name: 'лучник', health: 80},
+];
+
+const correctHeroes = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test('Проверка сортировки массива', () => {
+  const result = sortHeroes(heroes);
+  expect(result).toEqual(correctHeroes);
+});
